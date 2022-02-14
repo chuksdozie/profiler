311cefdb0b2440a96afca45f8de08bfdc412dac3
chuksdozie <chuksdozie48@gmail.com>
2022-02-14 11:12:06 +0100

    database setup

--- stores/database.js
+++ stores/database.js
@@ -10,13 +10,13 @@ const sql = postgres(process.env.DATABASE_URL, {
       console.log({ query: queryString, params });
     }
   },
-  // ssl: true,
+  ssl: true,
   requestCert: false,
   rejectUnauthorized: false,
   strictSSL: false,
   dialectOptions: {
     ssl: {
-      require: true, // This will help you. But you will see nwe error
+      // require: true, // This will help you. But you will see nwe error
       rejectUnauthorized: false, // This line will fix new error
     },
   },

03cca1c98363cf20b3cc0bb8d902f75a451a8bbd
chuks dozie <chuksdozie48@gmail.com>
2022-07-08 18:27:15 +0100

    SSL Fix for both dev and prod

--- stores/database.js
+++ stores/database.js
@@ -3,24 +3,38 @@ var env = require("dotenv");
 
 const IN_PROD = process.env.NODE_ENV === "production";
 
-const sql = postgres(process.env.DATABASE_URL, {
-  debug: (_, query, params) => {
-    if (!IN_PROD) {
-      const queryString = query.split("\n").join("");
-      console.log({ query: queryString, params });
-    }
-  },
-  ssl: true,
-  requestCert: false,
-  rejectUnauthorized: false,
-  // strictSSL: false,
-  dialectOptions: {
-    ssl: {
-      // require: true, // This will help you. But you will see nwe error
-      rejectUnauthorized: false, // This line will fix new error
+let sql;
+
+if (IN_PROD) {
+  sql = postgres(process.env.DATABASE_URL, {
+    ssl: true,
+    requestCert: false,
+    rejectUnauthorized: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
     },
-  },
-});
+  });
+} else {
+  sql = postgres(process.env.DATABASE_URL, {
+    debug: (_, query, params) => {
+      if (!IN_PROD) {
+        const queryString = query.split("\n").join("");
+        console.log({ query: queryString, params });
+      }
+    },
+    requestCert: false,
+    rejectUnauthorized: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
+  });
+}
 
 const testDBConnection = async () => {
   sql`SELECT 1+1 AS result`
